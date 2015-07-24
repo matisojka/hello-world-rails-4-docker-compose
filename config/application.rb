3ab3092ba2b2feb89dd0847dc922fbe9528ec21a
Mati Sojka <yagooar@gmail.com>
2015-07-24 20:16:42 +0300

    Allow web console for hosts other than localhost

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module HelloWorldRails
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.web_console.whitelisted_ips = '192.168.0.0/16'
   end
 end
